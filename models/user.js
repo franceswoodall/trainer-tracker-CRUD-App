105edceac46893e48d5a865ec5ebd57b7798aedd
Frances Woodall <franceswoodall@hotmail.co.uk>
2026-01-19 11:33:08 +0000

    POST route for sign up made, testing failed

--- models/user.js
+++ models/user.js
@@ -4,12 +4,12 @@ const userSchema = mongoose.Schema({
     username: {
         type: String, 
         required: true, 
-    }
+    }, 
     password: {
         type: String, 
         required: true,
     }, 
-}), 
+}); 
 
 const User = mongoose.model('User', userSchema); 
 
