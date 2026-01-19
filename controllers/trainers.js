9ccd5cd19cd02c2650d841349705746f27539af2
Frances Woodall <franceswoodall@hotmail.co.uk>
2026-01-19 14:26:22 +0000

    conditional rendering added in nav bar for links based on authentication

--- controllers/trainers.js
+++ controllers/trainers.js
@@ -5,7 +5,15 @@ const User = require('../models/user.js');
 const bcrypt = require('bcrypt'); 
 
 router.get('/', async (req, res) => {
-    res.render('trainers/index.ejs'); 
+    try {
+        const allTrainers = await Trainer.find({}); 
+        res.render('trainers/index.ejs', {
+            trainers: allTrainers, 
+            user: req.session.user
+        }); 
+    } catch (error) {
+        res.redirect('/'); 
+    }
 }); 
 
 module.exports = router; 
